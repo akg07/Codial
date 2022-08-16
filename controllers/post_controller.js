716398b3936e42d094eeba416b0817a4db8b1362
Ayush <ayush3032@gmail.com>
2022-08-17 02:32:04 +0530

    add controllers

--- controllers/post_controller.js
+++ controllers/post_controller.js
@@ -1,11 +1,16 @@
 module.exports.recient = function(req, res) {
 
 
-    return res.end('<h1>Recient Posts Sections</h1>')
+    // return res.end('<h1>Recient Posts Sections</h1>')
+    return res.render('Home', {
+        title: "Recient Post Page"
+    })
 }
 
 module.exports.feed = function(req, res) {
 
 
-    return res.end('<h1>My Feed page</h1>')
+    return res.render('feed', {
+        title: "My Feed"
+    })
 }
--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,10 +1,16 @@
 module.exports.profile = function(req, res) {
 
 
-    return res.end('<h1>Users Profile</h1>');
+    // return res.end('<h1>Users Profile</h1>');
+    return res.render('user', {
+        title: ""
+    })
 }
 
 module.exports.friends = function(req, res) {
 
-    return res.end('<h1>All My Friends List</h1>')
+    // return res.end('<h1>All My Friends List</h1>')
+    return res.render('user', {
+        title: "Friends"
+    })
 }
