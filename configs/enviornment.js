51306820ca7ca6f99704c13ae0adcee5a9f446a3
Ayush <ayush3032@gmail.com>
2022-09-20 01:04:00 +0530

    add production environment

--- configs/enviornment.js
+++ configs/enviornment.js
@@ -21,4 +21,26 @@ const development = {
     jwt_secert: 'codial',
 }
 
-module.exports = development;
+const production = {
+    name: 'production',
+    asset_path: process.env.CODIAL_ASSET_PATH,
+    session_cookie: process.env.CODIAL_SESSION_COOKIE,
+    db: process.env.CODIAL_DB,
+    smtp: {
+        service: 'gmail',
+        host: 'smtp.gmail.com',
+        port: 587,
+        secure: false,
+        auth: {
+            user: process.env.CODIAL_GMAIL_USERNAME,
+            pass: process.env.CODIAL_USER_PASS
+        }
+    },
+    google_client_id: process.env.CODIAL_GOOGLE_CLIENT_ID,
+    google_client_secret: process.env.CODIAL_GOOGLE_CLIENT_SECERT,
+    google_callback_url: process.env.CODIAL_GOOGLE_CALLBACK_URL,
+    jwt_secert: process.env.CODIAL_JWT_SECERT,
+}
+
+// module.exports = production;
+module.exports = eval(process.env.CODIAL_ENV) == undefined ? development : eval(process.env.CODIAL_ENV);
